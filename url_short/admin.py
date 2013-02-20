95e82a493b8837700782721c9a931a91606b78c3
Jacqui B <jacqueline.bascetta@gmail.com>
2013-02-20 14:16:35 -0500

    First commit

---
+++ url_short/admin.py
@@ -0,0 +1,6 @@
+__author__ = 'jacqui'
+
+from django.contrib import admin
+from url_short.models import ShortLink
+
+admin.site.register(ShortLink)
---
+++ url_short/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+import random
+
+
+class ShortLink(models.Model):
+    original_url = models.CharField(max_length=2048)
+    #mini_url = models.CharField(max_length=20, primary_key=True)
+
+    def randomize(self):
+        chars = ''.join(random.sample("abcdefghijklmnopqrstuvwxyz1234567890", 7))
+        return chars
+
+    mini_url = randomize
+
+    def __unicode__(self):
+        return self.original_url + ", " + self.mini_url
+
+
+
