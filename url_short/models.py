1fdb5497dc0e1ca700131afae3714a4ac9cd2035
JacquiBascetta <jacqueline.bascetta@gmail.com>
2013-02-25 16:18:56 -0500

    #Adding original URL as form

--- url_short/models.py
+++ url_short/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 import random
+from django import forms
 
 
 class ShortLink(models.Model):
-    original_url = models.CharField(max_length=2048)
-    #mini_url = models.CharField(max_length=20, primary_key=True)
+    original_url = models.URLField(verify_exists - True, unique = True)
+    mini_url = models.CharField(max_length=20, primary_key=True, unique = True)
 
-    def randomize(self):
+    def __init__(self, *args, **kwargs):
+        """
+
+        :param args:
+        :param kwargs:
+        """
+        kwargs.setdefault('mini_url', self.randomize())
+        super(ShortLink, self).__init__(*args, **kwargs)
+
+    def randomize(self, mini_url):
         chars = ''.join(random.sample("abcdefghijklmnopqrstuvwxyz1234567890", 7))
         return chars
 
     def __unicode__(self):
-        return self.original_url #+ ", " + self.mini_url
+        return self.original_url + ", " + self.mini_url
+
+class LinkForm(forms.Form):
+    u = forms.URLField(verify_exists = True,
+                       label = 'What URL would you like to shorten?',
+                       )
+
+
+
