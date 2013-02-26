038fd5a2000023214cf3e731b38f070d53d26359
JacquiBascetta <jacqueline.bascetta@gmail.com>
2013-02-26 15:23:37 -0500

    Updating to use forms

--- url_short/models.py
+++ url_short/models.py
@@ -1,30 +1,20 @@
 from django.db import models
 import random
+from django.forms import ModelForm
 from django import forms
 
 
 class ShortLink(models.Model):
-    original_url = models.URLField(verify_exists=True, unique=True)
-    mini_url = models.CharField(max_length=20, primary_key=True, unique=True)
+    original_url = models.URLField('What URL would you like to shorten?')
+    mini_url = models.CharField(max_length=20)
 
-    def __init__(self, *args, **kwargs):
-        """
-
-        :param args:
-        :param kwargs:
-        """
-        kwargs.setdefault('mini_url', self.randomize())
-        super(ShortLink, self).__init__(*args, **kwargs)
-
-    def randomize(self, mini_url):
+    def randomize(self):
         chars = ''.join(random.sample("abcdefghijklmnopqrstuvwxyz1234567890", 7))
         return chars
 
-    def __unicode__(self):
-        return self.original_url + ", " + self.mini_url
+class ShortLinkForm(ModelForm):
+    class meta:
+        model = ShortLink
+        fields = 'original_url'
 
 
-class LinkForm(forms.Form):
-    u = forms.URLField(verify_exists=True,
-                       label='What URL would you like to shorten?',
-                       )
--- url_short/views.py
+++ url_short/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.shortcuts import render
+from url_short.models import ShortLinkForm
+from django.http import HttpResponseRedirect
+
+
+def find_origurl(request, template_name='findurl/my_form.html'):
+    if request.method == 'POST': #if a form has been submitted
+        form  = ShortLinkForm(request.POST)
+        if form.is_valid():
+
+            url = form.save()
+            return HttpResponseRedirect('/thanks') #filled out properly, redirects
+    else:
+        form = ShortLinkForm() #gives unbound form
+    return render(request, template_name, {'form': form})
+
+
+
