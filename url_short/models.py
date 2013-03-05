fe0fd580b9dfe4948e9d0cb107e802fc0a8a5c85
JacquiBascetta <jacqueline.bascetta@gmail.com>
2013-03-05 16:20:52 -0500

    #formatting forms

--- url_short/models.py
+++ url_short/models.py
@@ -15,7 +15,12 @@ class ShortLink(models.Model):
     def __unicode__(self):
         return self.original_URL
 
+
 class ShortLinkForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ShortLinkForm, self).__init__(*args, **kwargs)
+        self.fields['original_URL'].label = ""
+
     class Meta:
         model = ShortLink
         exclude = ('mini_URL',)
